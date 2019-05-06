033dbc016a20ecdafe5f072c8ba3e7402f4d4e43
LILeilei <40779351+LILeilei66@users.noreply.github.com>
2019-05-06 18:00:55 +0800

    Add files via upload

--- 10layers.py
+++ 10layers.py
@@ -5,18 +5,38 @@ tf.keras.layers.Dense:
     While calling this object, the arguments should of dimension of two, with the shape[0] the
     instance size, and shape[1] the feature size.
 
+difference between tf.layers and tf.keras.layers:
+==================================================
+    tf.layers is the wrappers of tf.keras.layers, e.g.:
+        @tf_export('layers.Dense')
+        class Dense(keras_layers.Dense, base.Layer):
 
+why len(tf.layers.Dense.weight) is 2?
+=====================================
+    <class 'list'>:
+    [<tf.Variable 'dense/kernel:0' shape=(10, 100) dtype=float32, numpy=array(), dtype=float32)>,
+    <tf.Variable 'dense/bias:0' shape=(100,) dtype=float32, numpy=array(), dtype=float32)>]
+    答: 在 tf, weight 不叫 weight, 叫 kernel.
 """
 
 import tensorflow as tf
 tf.enable_eager_execution()
 
 # <editor-fold desc="利用 input 确定input shape of layer">
-layer = tf.keras.layers.Dense(100) # first argument signifies the output channel.
-print(len(layer.weights))
-inputs = tf.random_normal(shape=[10,10], mean=0.0)
+layer = tf.keras.layers.Dense(100, input_shape=[None, 10]) # first argument
+
+
+# signifies the output
+# channel.
+
+inputs = tf.random_normal(shape=[1,10], mean=0.0)
 outputs = layer(inputs)
-print(outputs)
+
+
+result = inputs.numpy() * layer.kernel.numpy() + layer.bias.numpy()
+print(result - outputs)
+print(type(layer.bias))
+# print(outputs)
 print((layer.weights[0].shape))
 inputs2 = tf.random_normal(shape=[100,10],mean=0.0)
 outputs2 = layer(inputs2)
