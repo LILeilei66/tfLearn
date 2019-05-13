eb8db66e440b43baf5dc90c4bf37800c2f2bf386
LILeilei <40779351+LILeilei66@users.noreply.github.com>
2019-05-14 01:35:14 +0800

    weight todo
    未完成

--- 6_2grad.py
+++ 6_2grad.py
@@ -4,6 +4,7 @@ ref: https://blog.csdn.net/xierhacker/article/details/53174558
 import tensorflow as tf
 
 # <editor-fold desc="1. 一元求导">
+print('1.')
 x = tf.constant(3.0)
 with tf.GradientTape(persistent=True) as tape:
     tape.watch(x)
@@ -24,16 +25,39 @@ print('dy3_dx', dy3_dx) # dy3_dx Tensor("AddN:0", shape=(), dtype=float32)
 
 # </editor-fold>
 
+# <editor-fold desc="1.2. 矩阵求导">
+print('1.2')
+x = tf.Variable(initial_value=[[1, 3]])
+w = tf.Variable(initial_value=[[2]])
+res = tf.matmul(w, x)
+init_op = tf.initialize_all_variables()
+
+with tf.Session() as sess:
+    sess.run(init_op)
+    res = sess.run(res)
+    # w = sess.run(w)
+    print(res)
+
+    with tf.GradientTape() as tape:
+        tape.watch(w)
+        dw = tape.gradient(res, w)
+    print(dw)
+
+# </editor-fold>
+
 # <editor-fold desc="2. Layer 求导">
-x = tf.constant([[2., 3.]])
+# todo: 始终没有成功提取 weight
+print('2.')
+x = tf.constant([[1.]])
 a = tf.keras.layers.Dense(2)
 b = tf.keras.layers.Dense(2)
 
 with tf.GradientTape(persistent=True) as tape:
     tape.watch(x)
-    result = b(a(x))
-    g_a = tape.gradient(a(x), x)
-    g_b = tape.gradient(result, x)
+    res_b = b(a(x))
+    res_a = a(x)
+g_a = tape.gradient(res_a, x)
+g_b = tape.gradient(res_b, x)
 
 init_op = tf.initialize_all_variables()
 
@@ -41,19 +65,21 @@ with tf.Session() as sess:
     sess.run(init_op)
     # v_a = sess.run(a.get_weights())
     # v_b = sess.run(b.get_weights())
-    output_a = sess.run(a(x))
-    result = sess.run(result)
+    res_b = sess.run(res_b)
+    res_a = sess.run(res_a)
     g_a = sess.run(g_a)
     g_b = sess.run(g_b)
 
-print('v_a', a.get_weights())
+print('x', x)
+print('v_a', a.kernel)
+# print('')
 """v_a [array([[-0.40425915, -0.59929955],
        [-0.95141447,  0.4541161 ]], dtype=float32), array([0., 0.], dtype=float32)]"""
 print('v_b', b.get_weights())
 """v_b [array([[-1.2015895 ,  0.3204056 ],
        [ 0.47647798,  0.54431033]], dtype=float32), array([0., 0.], dtype=float32)]"""
-print('output_a', output_a) # output_a output_a [[ 5.505309  -1.9712033]]
-print('result', result) # result [[-0.77420396 -5.688115  ]]
+print('output_a', res_a) # output_a output_a [[ 5.505309  -1.9712033]]
+print('result', res_b) # result [[-0.77420396 -5.688115  ]]
 print('g_a', g_a) # g_a [[1.253601   0.34230137]]
 print('g_b', g_b) # g_b [[-0.45510265 -1.8507048 ]]
 
